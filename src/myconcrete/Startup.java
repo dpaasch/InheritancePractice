2c2c1a8e132e53b2be7c24b26a9443a901b08119
dpaasch@my.wctc.edu <>
2013-01-26 14:43:33 -0600

    Update #6 - Added HourlyEmployee information to the Startup.java class, including an if statement to determine if employee is Salaried or not.

--- src/myconcrete/Startup.java
+++ src/myconcrete/Startup.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Startup {
 
     public static void main(String args[]) {
-       
+
         // Create a Scanner object for keyboard input.
         Scanner input = new Scanner(System.in);
 
@@ -27,17 +27,35 @@ public class Startup {
         System.out.println("Employee Department: ");
         String dept = input.nextLine();
 
-        // Get the Salaried employee information
-        System.out.println("Employee Annual Salary: ");
-        double salary = input.nextDouble();
+        // Query Employee status - Salaried or hourly
+        System.out.println("Is Employee Salaried (Yes or No)? ");
+        String choice = input.nextLine();   // Holds answer to Salary question
+        if ("yes".equals(choice) || "Yes".equals(choice)) {
+            // Get the Salaried employee information
+            System.out.println("Employee Annual Salary: ");
+            double salary = input.nextDouble();
 
-        // Create a SalariedEmployee object
-        SalariedEmployee sEmployee = new SalariedEmployee(salary, num, lName, 
-                fName, dept);
+            // Create a SalariedEmployee object
+            SalariedEmployee sEmployee = new SalariedEmployee(salary, num, lName,
+                    fName, dept);
 
-        // Display Employee/SalariedEmployee information
-        System.out.println("\n\n\n\n");
-        System.out.println("******Employee Information******");
-        System.out.println(sEmployee);
+            // Display Employee/SalariedEmployee information
+            System.out.println("\n\n\n\n");
+            System.out.println("******Employee Information******");
+            System.out.println(sEmployee);
+        } else {
+            // Get the Hourly employee information
+            System.out.println("Employee Hourly Rate: ");
+            double rate = input.nextDouble();
+
+            // Create a SalariedEmployee object
+            HourlyEmployee hEmployee = new HourlyEmployee(rate, num, lName,
+                    fName, dept);
+
+            // Display Employee/HourlyEmployee information
+            System.out.println("\n\n\n\n");
+            System.out.println("******Employee Information******");
+            System.out.println(hEmployee);
+        }
     }
 }
