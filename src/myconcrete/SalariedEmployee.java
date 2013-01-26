c744bb80003672951ed6188c69b2fbad6759c5fe
dpaasch@my.wctc.edu <>
2013-01-26 14:03:50 -0600

    Update #2 - Removed all code from Startup.java & SalariedEmployee.java, as both were producing errors on the run of the Startup.java.

--- src/myconcrete/SalariedEmployee.java
+++ src/myconcrete/SalariedEmployee.java
@@ -3,54 +3,6 @@
  */
 package myconcrete;
 
-import java.text.DecimalFormat;
-
-public class SalariedEmployee extends Employee {
-
-    // SalariedEmployee Class fields 
-    private double EmployeeSalary;     // Employee salary
-
-    // Constructor: Accepts as arguments the employee salary, along with 
-    // the inherited fields from the Employee class: employee number, last
-    // name, first name, hire date, and department.
-    public SalariedEmployee(double salary, String num, String lName, String fName, 
-            String dept) {
-        // Call the superclass constructor
-        super(num, lName, fName, dept);
-        // Set the eSalary field.
-        EmployeeSalary = salary;
-    }
-    
-    // Constructor: Accepts as arguments the employee's salary
-    public SalariedEmployee(double salary) {
-        EmployeeSalary = salary;
-    }
-
-    // No-arg Constructor: 
-    public SalariedEmployee() {
-    }
-
-    // Method toString(): represents the state of an object
-    @Override
-    public String toString() {
-
-        // Create a DecimalFormat object to better display the salary
-        DecimalFormat dollar = new DecimalFormat("###,##0.00");
-
-        String salariedEmployee;
-        salariedEmployee = super.toString() 
-         + "\n  Salaried Employee Annual Salary: $" + dollar.format(EmployeeSalary);
-        // Return the string.
-        return salariedEmployee;
-    }
-    
-    // Accessor & Mutator Methods
-    public double getEmployeeSalary() {
-        return EmployeeSalary;
-    }
-
-    public void setEmployeeSalary(double EmployeeSalary) {
-        this.EmployeeSalary = EmployeeSalary;
-    }
+public class SalariedEmployee {
 
 }
--- src/myconcrete/Startup.java
+++ src/myconcrete/Startup.java
@@ -3,43 +3,9 @@
  */
 package myconcrete;
 
-import java.util.Scanner;
-
 public class Startup {
 
     public static void main(String args[]) {
-
-        // Create a Scanner object for keyboard input.
-        Scanner input = new Scanner(System.in);
-
-        // Get the emmployee number
-        System.out.println("Enter the employee number in the format of XX");
-        System.out.println("Employee Number: ");
-        String num = input.nextLine();
-
-        // Get the employee last name, first name
-        System.out.println("Employee Last Name: ");
-        String lName = input.nextLine();
-        System.out.println("Employee First Name: ");
-        String fName = input.nextLine();
-
-        // Get the employee department
-        System.out.println("Employee Department: ");
-        String dept = input.nextLine();
-
-        // Get the Salaried employee information
-        System.out.println("Employee Annual Salary: ");
-        double salary = input.nextDouble();
-
-        // Create a new Salaried Employee object.
-        SalariedEmployee salariedEmployee = new SalariedEmployee(salary, num, lName,
-                fName, dept);
-
-        // Display Employee/SalariedEmployee information
-        System.out.println("\n\n\n\n");
-        System.out.println("******Employee Information******");
-        System.out.println(salariedEmployee);
-
-
+        
     }
 }
