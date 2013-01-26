4d2aa7b597c6c03e6b095c9aa91d03b05acdd844
dpaasch@my.wctc.edu <>
2013-01-26 14:11:10 -0600

    Update #3- Re-added code for SalariedEmployee.java.

--- src/myconcrete/SalariedEmployee.java
+++ src/myconcrete/SalariedEmployee.java
@@ -3,6 +3,48 @@
  */
 package myconcrete;
 
-public class SalariedEmployee {
+import java.text.DecimalFormat;
 
+public class SalariedEmployee extends Employee {
+
+    // SalariedEmployee class fields
+    private double eSalary;     // Employee annual salary
+
+    // Constructor: Accepts as arguments the employee's annual salary, along with
+    // the inherited fields employee number, last name, first name, and department.
+    public SalariedEmployee(double salary, String num, String lName, String fName,
+            String dept) {
+        // Call the superclass constructor
+        super(num, lName, fName, dept);
+        // Set the eSalary field.
+        eSalary = salary;
+    }
+    
+    // no-arg constructor
+    public SalariedEmployee() {
+    }
+
+    // Method toString(): represents the state of an object
+    @Override
+    public String toString() {
+
+        // Create a DecimalFormat object to better display the salary
+        DecimalFormat dollar = new DecimalFormat("###,##0.00");
+
+        String salariedEmployee;
+        salariedEmployee = super.toString() 
+         + "\n  Salaried Employee Annual Salary: $" + dollar.format(eSalary);
+        // Return the string.
+        return salariedEmployee;
+    }
+    
+    // Accessor & Mutator Methods
+    public double geteSalary() {
+        return eSalary;
+    }
+
+    public void seteSalary(double eSalary) {
+        this.eSalary = eSalary;
+    }
+    
 }
