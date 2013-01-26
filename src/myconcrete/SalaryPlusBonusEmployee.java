4d1d2953026a1a2cd64982113bbb32c00486fab0
dpaasch@my.wctc.edu <>
2013-01-26 14:56:41 -0600

    Update #7 - Added code to SalaryPlusBonusEmmployee.java class.

--- src/myconcrete/SalaryPlusBonusEmployee.java
+++ src/myconcrete/SalaryPlusBonusEmployee.java
@@ -3,7 +3,48 @@
  */
 package myconcrete;
 
+import java.text.DecimalFormat;
+
 public class SalaryPlusBonusEmployee extends SalariedEmployee {
 
+    // SalaryPlusBonusEmployee fields
+    double eBonus;  // Salaried employee bonus
+    
+    // Constructor: Accepts salaried employee bonus, along with the SalariedEmployee
+    // inherited field salary and the Employee inherited fields.
+    public SalaryPlusBonusEmployee(double eBonus, double salary, String num, 
+            String lName, String fName, String dept) {
+        super(salary, num, lName, fName, dept);
+        this.eBonus = eBonus;
+    }
+    
+    // Constructor: Accepts only the Employee inherited fields.
+    public SalaryPlusBonusEmployee(String num, String lName, String fName, String dept) {
+        super(num, lName, fName, dept);
+    }
+
+    // Method toString(): represents the state of an object
+    @Override
+    public String toString() {
+
+        // Create a DecimalFormat object to better display the salary
+        DecimalFormat dollar = new DecimalFormat("###,##0.00");
+
+        String salaryPlusBonusEmployee;
+        salaryPlusBonusEmployee = super.toString() 
+         + "\n  Salaried Employee Bonus: $" + dollar.format(eBonus);
+        // Return the string.
+        return salaryPlusBonusEmployee;
+    }
+    
+    // Accessor & Mutator methods:
+    public double geteBonus(double salary, double bonus) {
+        bonus = salary * .05;
+        return eBonus;
+    }
+
+    public void seteBonus(double eBonus) {
+        this.eBonus = eBonus;
+    }
     
 }
