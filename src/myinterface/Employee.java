ac4050841ffc96351cc7225656a4287d8b3fac33
dpaasch@my.wctc.edu <>
2013-01-26 18:30:46 -0600

    Update #15 - Added code to the Employee interface.

--- src/myinterface/Employee.java
+++ src/myinterface/Employee.java
@@ -5,4 +5,21 @@ package myinterface;
 
 public interface Employee {
     
+    // These methods will all need to be overridden by the classes.
+    public String geteNum();
+    
+    public void seteNum(String eNum);
+    
+    public String geteLastName();
+    
+    public void seteLastName(String eLastName);
+    
+    public String geteFirstName();
+    
+    public void seteFirstName(String eFirstName);
+    
+    public String geteDept();
+    
+    public void seteDept(String eDept);
+
 }
